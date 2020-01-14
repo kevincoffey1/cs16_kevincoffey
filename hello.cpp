e0c7ba3b54d754793061ab70391f9164f88e8543
kevincoffey1 <59663120+kevincoffey1@users.noreply.github.com>
2020-01-14 14:29:52 -0800

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+// "Include directive"
+// use the basic libraries for input and output 
+using namespace std; 
+// Use parts of the standard libraries in C++ without prepending the keyword "std::"
+//Opening terminal: ctrl-`
+// in terminal: 
+// ls
+// g++ [file name].cpp
+// ./a.out
+// g++ hello.cpp - o foobar (changes name to foobar)
+// ./foobar 
+int main() {
+	//Every C++ program must have a main function. Starting point of execution
+	cout<<"Hello CS16 Students"; 
+	// "print" function, in C++, anything in "" is a string 
+	cout<<endl; 
+	//Goes to a new line (end line)
+	return 0; 
+}
