7364bdbf8aae91f0299b9dcb7aee1b631332643b
kevincoffey1 <kevincoffey@ucsb.edu>
2020-01-14 14:58:53 -0800

    xd

---
+++ functions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void printHello(){
+cout<<"hello"<<endl; 
+}
+string returnHello(){
+	string res = "hello"; 
+	return res; 
+int main() {
+	printHello();
+        returnHello(); 	
+	return 0; 
+}
